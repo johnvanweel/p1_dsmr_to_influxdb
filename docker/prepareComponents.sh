5a8bb0ea7c86883aa4f982b15a5cdae37d68b601
John van Weel <john@johnvanweel.nl>
2020-07-26 18:11:08 +0200

    Clean up commented code

--- docker/prepareComponents.sh
+++ docker/prepareComponents.sh
@@ -1,6 +1,5 @@
 rm components/*
 
-#wget https://raw.githubusercontent.com/psy0rz/p1_dsmr_to_influxdb/master/p1_to_influxdb.py -P ./components/
 cp ../src/p1_to_influxdb2.py components
 cp ../src/config.py components
 cp Dockerfile components
